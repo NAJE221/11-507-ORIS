e3cd0e9bf939b564a37ef286860e0dbe727979da
NAJE221 <artemigorevich113@gmail.com>
2026-09-25 20:51:50 +0300

    Update .gitignore

--- 26.09.26/http-server/http-server/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿
-using http_server;
-
-HttpServer server = new HttpServer();
-server.Run();
--- 26.09.26/http-server/http-server/Server.cs
+++
@@ -1,7 +0,0 @@
-﻿public class Server
-{
-    public string host { get; set; } = "127.0.0.1";
-    public string port { get; set; } = "8080";
-    public string path { get; set; } = "content/";
-}
-
